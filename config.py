242d6ac6160489dabffb10af4fd8e4d2338a48c1
hxracan <notreal101001@gmail.com>
2026-07-13 19:24:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-# Global configuration
-MAX_CONCURRENT_REQUESTS = 1000
-SPAM_MESSAGE = "NUKE | LIGHTSPEED"
 COMMAND_PREFIX = "!"
 TOKENS_FILE = "tokens.txt"
+SPAM_MESSAGE = "**THIS SERVER HAS BEEN FUCKED BY HURACAN**"
+GUILD_ID = None  # Set to guild ID if you want to lock to a single server, otherwise None
