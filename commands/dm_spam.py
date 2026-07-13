9432cfca930306cf582b11f862d96d939686faed
hxracan <notreal101001@gmail.com>
2026-07-13 19:27:15 +0200

    Update dm_spam.py

--- commands/dm_spam.py
+++ commands/dm_spam.py
@@ -1,1 +1,18 @@
-# mass DM, webhook spam
+import asyncio
+
+class DmSpamCommands:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def massdm(self, guild, message):
+        members = [m for m in guild.members if m != guild.me and not m.bot]
+        tasks = []
+        for member in members:
+            tasks.append(self.send_dm(member, message))
+        await asyncio.gather(*tasks, return_exceptions=True)
+
+    async def send_dm(self, member, message):
+        try:
+            await member.send(message)
+        except:
+            pass
